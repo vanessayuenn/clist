23dfba773b116483d3b2e08b6c34fb181c9b3c63
Vanessa Yuen <vanessa.yh.yuen@gmail.com>
2017-04-05 22:11:31 -0400

    make cli

--- cli.js
+++ cli.js
@@ -1,5 +1,49 @@
 #! /usr/bin/env node
 
 
+const Https = require('https');
+const Chalk = require('Chalk');
+const Config = require('./config');
+
 const userArgs = process.argv.slice(2);
-console.log(userArgs);
+
+
+const printData = (content) => {
+
+  console.log(`\n\t🥑  ${ Chalk.bold.underline.green('your fabulous list') } 🥑\n`);
+
+  content.forEach((elem) => {
+    const itemsColors = {
+      id: elem.done ? Chalk.dim.strikethrough : Chalk.magenta,
+      content: elem.done ? Chalk.dim.strikethrough : Chalk.white,
+    }
+    const item = [
+      `  [${elem.done ? 'x' : ' '}]`,
+      itemsColors.id(elem._id),
+      itemsColors.content(elem.content || '')
+    ];
+    console.log(item.join('   '));
+  });
+
+  console.log(`\n\t🌊  ${Chalk.blue('kthxbai.\n')}`);
+}
+
+const req = Https.request({
+  host: Config.WEBTASK_HOST,
+  path: Config.WEBTASK_PATH,
+  headers: {
+    'Authorization': `Bearer ${Config.ACCESS_TOKEN}`,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  method: 'GET'
+}, (res) => {
+  res.setEncoding('utf8')
+  res.on('data', (data) => printData(JSON.parse(data).data));
+});
+
+req.on('error', function(e) {
+  console.error(e);
+});
+
+req.end();
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  WEBTASK_HOST: 'wt-vanessa_yh_yuen-gmail_com-0.run.webtask.io',
+  WEBTASK_PATH: '/cmdlist',
+  ACCESS_TOKEN: ''
+}
+
+return module.exports;
